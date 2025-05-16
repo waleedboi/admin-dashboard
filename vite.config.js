fc675b1c3a735a32c76d5fbfe88a77e3bb391d1f
waleedboi <waleedahmadkhan790@gmail.com>
2025-05-17 03:07:18 +0500

    code push by waleed

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/admin-dashboard',
+  // base: '/admin-dashboard',
   plugins: [react()],
 })
